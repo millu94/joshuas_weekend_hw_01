c8a99e5694a7f99bd9ce304935abe9b6223d3142
Joshua <jssmiller94@gmail.com>
2021-04-03 17:34:52 +0100

    function 3 works with test 3 & 4

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -5,9 +5,18 @@ def get_pet_shop_name(pet_shop_info):
     name = pet_shop_info["name"]
     return name
 
+#2 find the total cash
 def get_total_cash(pet_shop_info):
     total_cash = pet_shop_info["admin"]["total_cash"]
     return total_cash
+
+#3 + #4 add or remove cash
+def add_or_remove_cash(pet_shop_info, cash_amount):
+    pet_shop_info["admin"]["total_cash"] = pet_shop_info["admin"]["total_cash"] + cash_amount
+
+
+
+
 
 
 
--- tests/pet_shop_test.py
+++ tests/pet_shop_test.py
@@ -84,13 +84,13 @@ class TestPetShop(unittest.TestCase):
         sum = get_total_cash(self.cc_pet_shop)
         self.assertEqual(1000, sum)
 
-    @unittest.skip("delete this line to run the test")
+#function 3
     def test_add_or_remove_cash__add(self):
         add_or_remove_cash(self.cc_pet_shop,10)
         cash = get_total_cash(self.cc_pet_shop)
         self.assertEqual(1010, cash)
 
-    @unittest.skip("delete this line to run the test")
+#function 4
     def test_add_or_remove_cash__remove(self):
         add_or_remove_cash(self.cc_pet_shop,-10)
         cash = get_total_cash(self.cc_pet_shop)
